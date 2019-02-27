b36ea5325c42404e30dd1bfc3c08280a85e86c97
SarahLechler <s_lech05@uni-muenster.de>
2019-02-27 11:45:03 +0100

    update inception Year to 2019

--- src/main/js/bundles/dn_featureinfohighlight/FeatureHighlightFactory.js
+++ src/main/js/bundles/dn_featureinfohighlight/FeatureHighlightFactory.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 con terra GmbH (info@conterra.de)
+ * Copyright (C) 2019 con terra GmbH (info@conterra.de)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 export default class FeatureInfoHighlighter {
     createInstance() {
         let mapWidgetModel = this._mapWidgetModel;
